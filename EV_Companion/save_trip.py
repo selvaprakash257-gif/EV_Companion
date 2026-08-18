8883306a22c031b562c13fb506f3a324b780d8aa
Selvaprakash <selvaprakash257@gmail.com>
2026-08-18 07:14:59 +0530

    Convert trip battery input to integer

--- EV_Companion/save_trip.py
+++ EV_Companion/save_trip.py
@@ -5,7 +5,7 @@ def save_trip(logged_user):
     user_id = logged_user[0]
 
     vehicle = input("Enter Vehicle Model: ")
-    battery = input("Enter Battery Percentage: ")
+    battery = int(input("Enter Battery Percentage: ")) 
     destination = input("Enter Destination: ")
 
     save_trip_to_database(
